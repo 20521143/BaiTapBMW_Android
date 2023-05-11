fbe3810dcf9cf3aab77bd03ebaae32a9d45c7056
CongNguyen123 <nguyenthanhlong012002@gmail.com>
2023-05-11 13:27:15 +0700

    add full code

--- app/src/main/java/com/camoi/baitapadroidSqlite/LoginActivity.java
+++ app/src/main/java/com/camoi/baitapadroidSqlite/LoginActivity.java
@@ -4,34 +4,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button btnLogin;
-    private Button btnSignup;
+
+    DataBaseHelper dataBaseHelper;
+    private Button btn_login, btn_signup;
+    private EditText et_username, et_password;
+
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btnLogin = (Button) findViewById(R.id.btn_login);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_signup = (Button) findViewById(R.id.btn_signup);
+        et_username = (EditText) findViewById(R.id.et_username);
+        et_password = (EditText) findViewById(R.id.et_password);
+
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, HelloActivity.class);
-                startActivity(intent);
+
+                dataBaseHelper = new DataBaseHelper(LoginActivity.this);
+
+                UserModel user = dataBaseHelper.getUser(et_username.getText().toString(), et_password.getText().toString());
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, HelloActivity.class);
+                    startActivity(intent);
+                }
+
             }
         });
-        btnSignup = (Button) findViewById(R.id.btn_signup);
 
-        btnSignup.setOnClickListener(new View.OnClickListener() {
+        btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
+
+        //validate user
+
+
+
+
     }
 }
